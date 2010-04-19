c768f5684330ea4156e3a8d8fe4d8c523a68ef2a
Phlip <philip@cukerinteractive.com>
2010-04-19 11:52:34 -0700

    authorize_net

--- lib/merchant_gateways/billing/credit_card.py
+++ lib/merchant_gateways/billing/credit_card.py
@@ -1,21 +1,6 @@
 import re
 from datetime import datetime
 
-CARD_COMPANIES = {
-    'visa': '^4\d{12}(\d{3})?$',
-    'master': '^(5[1-5]\d{4}|677189)\d{10}$',
-    'discover': '^(6011|65\d{2})\d{12}$',
-    'american_express': '^3[47]\d{13}$',
-    'diners_club': '^3(0[0-5]|[68]\d)\d{11}$',
-    'jcb': '^3528\d{12}$',
-    'switch': '^6759\d{12}(\d{2,3})?$',
-    'solo': '^6767\d{12}(\d{2,3})?$',
-    'dankort': '^5019\d{12}$',
-    'maestro': '^(5[06-8]|6\d)\d{10,17}$',
-    'forbrugsforeningen': '^600722\d{10}$',
-    'laser': '^(6304[89]\d{11}(\d{2,3})?|670695\d{13})$'
-}
-
 class CreditCard(object):
     require_verification_value = True
 
@@ -258,3 +243,19 @@ class CreditCard(object):
 #             valid_test_mode_card_number?(number) #:nodoc:
 #             valid_checksum?(number) '''
 #
+
+CARD_COMPANIES = {
+    'visa': '^4\d{12}(\d{3})?$',
+    'master': '^(5[1-5]\d{4}|677189)\d{10}$',
+    'discover': '^(6011|65\d{2})\d{12}$',
+    'american_express': '^3[47]\d{13}$',
+    'diners_club': '^3(0[0-5]|[68]\d)\d{11}$',
+    'jcb': '^3528\d{12}$',
+    'switch': '^6759\d{12}(\d{2,3})?$',
+    'solo': '^6767\d{12}(\d{2,3})?$',
+    'dankort': '^5019\d{12}$',
+    'maestro': '^(5[06-8]|6\d)\d{10,17}$',
+    'forbrugsforeningen': '^600722\d{10}$',
+    'laser': '^(6304[89]\d{11}(\d{2,3})?|670695\d{13})$'
+}
+
