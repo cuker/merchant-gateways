d4f58704d832b4e4fb68b8e549b56f48e2dc1a5a
Phlip <philip@cukerinteractive.com>
2010-04-20 16:20:36 -0700

    payflow

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "merchant_gateways",  #  TODO  merchant-gateways ?
+    version = "0.0.1",
+    author = "Cuker Interactive",
+    author_email = "TODO@morethanseven.net",
+    url = "http://github.com/cuker/merchant_gateways/",
+
+    packages = find_packages('lib'),
+    package_dir = {'':'lib'},
+    license = "MIT License",
+    keywords = "django creditcard merchant authorization purchase",
+    description = "Unifying the diversity of gateways into a common interface",
+    install_requires=[
+        'setuptools',
+        'lxml'  #  TODO  what else?
+    ],
+    classifiers = [
+        "Intended Audience :: Developers",  # TODO  do these!
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+    ]
+)
