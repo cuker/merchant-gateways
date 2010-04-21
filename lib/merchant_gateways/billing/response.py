bdae9eb69dbfc787c8d288c5036b0f35a9f70039
Phlip <philip@cukerinteractive.com>
2010-04-21 15:07:26 -0700

    payflow

--- lib/merchant_gateways/billing/response.py
+++ lib/merchant_gateways/billing/response.py
@@ -11,7 +11,7 @@ class Response(object):
 
         self.options     = default_dict(options).set_default(None)
         self.test          = options.setdefault('test', False)
-        self.authorization = self.options['authorization']  #  TODO  just jam them in??
+        self.authorization = self.options['authorization']
         self.fraud_review  = self.options['fraud_review']
         self.is_test    = self.options['is_test']
         self.avs_result = self.options['avs_result']
@@ -20,7 +20,7 @@ class Response(object):
         if self.avs_result:  self.avs_result = AVSResult(code=self.avs_result)  #  TODO  document if it was not in the inbound message it's None
         if self.cvv_result:  self.cvv_result = CVVResult(code=self.cvv_result)
 
-    def to_dict(self):  #  TODO  give a crap about this
+    def to_dict(self):  #  TODO  find a use for this
         return {'success': self.success,
                 'message': self.message,
                 'params': self.params,
