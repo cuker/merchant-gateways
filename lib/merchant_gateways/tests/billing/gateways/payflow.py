3c31edfb51acaa1beec28cb57f403e5f48ee109c
Phlip <philip@cukerinteractive.com>
2010-04-22 12:56:31 -0700

    refactor reusables

---
+++ lib/merchant_gateways/tests/billing/gateways/payflow.py
@@ -0,0 +1,49 @@
+
+
+class MerchantGatewaysPayflowSuite:
+    
+    def assert_webservice_called(self):
+        args = self.gateway.post_webservice.call_args[0]
+        assert 2 == len(self.gateway.post_webservice.call_args), 'should be 1 but either we call twice or the Mock has an issue'
+        self.assert_equal('https://pilot-payflowpro.paypal.com', args[0])
+
+        xml = args[1]
+        xmlns = 'xmlns="http://www.paypal.com/XMLPay"' # TODO  pass to .xpath() instead of erasing!
+        assert xmlns in xml
+        xml = xml.replace(xmlns, '')
+
+        self.assert_xml(xml, lambda x:
+                x.XMLPayRequest(
+                        x.RequestData(
+                            x.Vendor('LOGIN'),
+                            x.Partner('PayPal'),
+                            x.Transactions(
+                                x.Transaction(
+                                    x.Verbosity('MEDIUM'),
+                                    x.Authorization()
+                                )
+                            )
+                        ),
+                        x.PayData(
+                            x.Invoice(
+                                x.TotalAmt('1.00', Currency='USD')
+                            ),
+                        x.Tender(
+                            x.Card(
+                                x.CardType('Visa'),
+                                x.CardNum('4242424242424242'),
+                                x.ExpDate('201109'),
+                                x.NameOnCard('Longbob'),
+                                x.CVNum('123'),
+                                x.ExtData(Name="LASTNAME", Value="Longsen")
+                            )
+                        )
+                    ),
+                    x.RequestAuth(
+                        x.UserPass(
+                            x.User('LOGIN'),
+                            x.Password('PASSWORD')
+                        )
+                    )
+                )
+            )
--- tests/unit/gateways/payflow_tests.py
+++ tests/unit/gateways/payflow_tests.py
@@ -2,62 +2,19 @@
 from merchant_gateways.billing.gateways.payflow import Payflow, format, xStr
 from merchant_gateways.billing.credit_card import CreditCard
 from merchant_gateways.tests.test_helper import *
+from merchant_gateways.tests.billing.gateways.payflow import MerchantGatewaysPayflowSuite
 from pprint import pprint
 
 
-class PayflowTests(MerchantGatewaysTestSuite, MerchantGatewaysTestSuite.CommonTests):
+class PayflowTests( MerchantGatewaysTestSuite,
+                    MerchantGatewaysTestSuite.CommonTests,
+                    MerchantGatewaysPayflowSuite ):
 
     def gateway_type(self):
         return Payflow
 
     def mock_webservice(self, response):
         self.mock_post_webservice(response)
-
-    def assert_webservice_called(self):
-        args = self.gateway.post_webservice.call_args[0]
-        assert 2 == len(self.gateway.post_webservice.call_args), 'should be 1 but either we call twice or the Mock has an issue'
-        self.assert_equal('https://pilot-payflowpro.paypal.com', args[0])
-
-        xml = args[1]
-        xmlns = 'xmlns="http://www.paypal.com/XMLPay"' # TODO  pass to .xpath() instead of erasing!
-        assert xmlns in xml
-        xml = xml.replace(xmlns, '')
-
-        self.assert_xml(xml, lambda x:
-                x.XMLPayRequest(
-                        x.RequestData(
-                            x.Vendor('LOGIN'),
-                            x.Partner('PayPal'),
-                            x.Transactions(
-                                x.Transaction(
-                                    x.Verbosity('MEDIUM'),
-                                    x.Authorization()
-                                )
-                            )
-                        ),
-                        x.PayData(
-                            x.Invoice(
-                                x.TotalAmt('1.00', Currency='USD')
-                            ),
-                        x.Tender(
-                            x.Card(
-                                x.CardType('Visa'),
-                                x.CardNum('4242424242424242'),
-                                x.ExpDate('201109'),
-                                x.NameOnCard('Longbob'),
-                                x.CVNum('123'),
-                                x.ExtData(Name="LASTNAME", Value="Longsen")
-                            )
-                        )
-                    ),
-                    x.RequestAuth(
-                        x.UserPass(
-                            x.User('LOGIN'),
-                            x.Password('PASSWORD')
-                        )
-                    )
-                )
-            )
 
     def assert_successful_authorization(self):
 
