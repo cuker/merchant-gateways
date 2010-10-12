ca6643d1791047976d31fd4fc7bbfd5a6eb06375
Jason Kraus <jasonk@cukerinteractive.com>
2010-10-12 10:26:36 -0700

    created test harness to work with hudson

--- tests/testrunner.py
+++ tests/testrunner.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 def runtests():
+    print 'start...'
     suite = unittest.defaultTestLoader.loadTestsFromNames(['tests.unit.avs_result_test',
                                                            'tests.unit.cvv_result_test',
                                                            'tests.unit.credit_card_tests',
@@ -16,9 +17,15 @@ def runtests():
     if os.environ.get('XML_OUTPUT', False):
         from xmlrunner import XMLTestRunner
         runner = XMLTestRunner()
+        print 'xml ftw'
     else:
         runner = unittest.TextTestRunner(verbosity=1, descriptions=False)
-    sys.exit(runner.run(suite).wasSuccessful() and 0 or 1)
+    print 'running'
+    result = runner.run(suite).wasSuccessful()
+    print result
+    exit_code = result and 0 or 1
+    print exit_code
+    sys.exit(exit_code)
 
 if __name__ == '__main__':
     runtests()
