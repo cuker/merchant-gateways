78894315f3ffbdce3888e4b59320c326a9b490a2
Jason Kraus <jasonk@cukerinteractive.com>
2011-02-09 14:23:46 -0800

    fixes for python 2.5

--- lib/merchant_gateways/tests/billing/gateways/braintree_orange_suite.py
+++ lib/merchant_gateways/tests/billing/gateways/braintree_orange_suite.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from __future__ import with_statement
 import datetime
 import merchant_gateways
 
--- lib/merchant_gateways/tests/billing/gateways/gateway_suite.py
+++ lib/merchant_gateways/tests/billing/gateways/gateway_suite.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import with_statement
 import unittest
 
 from merchant_gateways.billing.credit_card import CreditCard
