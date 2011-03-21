4ccca94c19bbd09dc84aab7ff5820b1c128a5056
Jason Kraus <jasonk@cukerinteractive.com>
2011-03-21 16:38:40 -0700

    cleaned up payflow

--- lib/merchant_gateways/billing/gateways/payflow.py
+++ lib/merchant_gateways/billing/gateways/payflow.py
@@ -20,16 +20,31 @@ class Payflow(Gateway):
     TEST_URL = 'https://pilot-payflowpro.paypal.com'
     LIVE_URL = 'https://payflowpro.paypal.com'
 
-    def authorize(self, money, credit_card, card_store_id=None, **options):  #  TODO  rename money to amount, everywhere
-        credit_card_or_reference = credit_card or card_store_id
-        request = self.build_sale_or_authorization_request('authorization', money, credit_card_or_reference, **options)
+    def authorize(self, money, credit_card=None, card_store_id=None, **options):
+        if card_store_id:
+            request = self.build_reference_sale_or_authorization_request('authorization', money, card_store_id, **options)
+        else:
+            request = self.build_credit_card_request('authorization', money, credit_card, **options)
+        return self.commit(request)
+    
+    def purchase(self, money, credit_card=None, card_store_id=None, **options):
+        if card_store_id:
+            request = self.build_reference_sale_or_authorization_request('purchase', money, card_store_id, **options)
+        else:
+            request = self.build_credit_card_request('purchase', money, credit_card, **options)
         return self.commit(request)
 
-    def build_sale_or_authorization_request(self, action, money, credit_card_or_reference, **options):  # TODO  tdd each arg
-        if isinstance(credit_card_or_reference, basestring):
-            return self.build_reference_sale_or_authorization_request(action, money, credit_card_or_reference, **options)
-        else:
-            return self.build_credit_card_request(action, money, credit_card_or_reference, **options)
+    def void(self, authorization, **options):
+        self.request = self.build_reference_request('void', None, authorization, **options)
+        return self.commit(self.request)
+    
+    def credit(self, money, authorization, **options):
+        request = self.build_reference_request('credit', money, authorization, **options)
+        return self.commit(request)
+
+    def capture(self, money, authorization, **options):
+        request = self.build_reference_request('capture', money, authorization, **options)
+        return self.commit(request)
 
     def commit(self, request_body, request_type = None):
         request = self.build_request(request_body, request_type)
@@ -52,12 +67,7 @@ class Payflow(Gateway):
             )
         return response
 
-    def purchase(self, money, credit_card, card_store_id=None, **options):  #  TODO every purchase can work on a cc or ref!
-        credit_card_or_reference = credit_card or card_store_id
-        self.message = self.build_sale_or_authorization_request('purchase', money, credit_card_or_reference, **options)
-        return self.commit(self.message)  #  TODO  test we return something
-
-    def build_headers(self, content_length):  #  TODO doesn't an HTTP library take care of this for us?
+    def build_headers(self, content_length):
         return {
           "Content-Type" : "text/xml",
           "Content-Length" : str(content_length),
@@ -225,10 +235,6 @@ xmlns="http://www.paypal.com/XMLPay">
                 )
             )
 
-    def void(self, authorization):
-        self.request = self.build_reference_request('void', None, authorization)
-        return self.commit(self.request)
-
     def invoice_total_amt(self, money):
         if not money:  return []
 
@@ -237,230 +243,6 @@ xmlns="http://www.paypal.com/XMLPay">
                         XML.TotalAmt( '%.2f' % money.amount, #  TODO currency-specific template!
                                       Currency=str(money.currency.code) ) )
         ]
-
-    def credit(self, money, identification_or_credit_card):
-        if True:  # TODO identification_or_credit_card.is_a?(String)
-          # Perform referenced credit
-          request = self.build_reference_request('credit', money, identification_or_credit_card)
-        else:
-          # Perform non-referenced credit
-          '# TODO request = build_credit_card_request(:credit, money, identification_or_credit_card, options)'
-
-        return self.commit(request)
-
-    def capture(self, money, authorization, **options):
-        request = self.build_reference_request('capture', money, authorization)
-        return self.commit(request)
-
-'''      include PayflowCommonAPI
-
-      RECURRING_ACTIONS = Set.new([:add, :modify, :cancel, :inquiry, :reactivate, :payment])
-
-      self.supported_cardtypes = [:visa, :master, :american_express, :jcb, :discover, :diners_club]
-      self.homepage_url = 'https://www.paypal.com/cgi-bin/webscr?cmd=_payflow-pro-overview-outside'
-      self.display_name = 'PayPal Payflow Pro'
-
-      def purchase(money, credit_card_or_reference, options = {})
-        request = build_sale_or_authorization_request(:purchase, money, credit_card_or_reference, options)
-
-        commit(request)
-      end
-
-      def credit(money, identification_or_credit_card, options = {})
-        if identification_or_credit_card.is_a?(String)
-          # Perform referenced credit
-          request = build_reference_request(:credit, money, identification_or_credit_card, options)
-        else
-          # Perform non-referenced credit
-          request = build_credit_card_request(:credit, money, identification_or_credit_card, options)
-        end
-
-        commit(request)
-      end
-
-      # Adds or modifies a recurring Payflow profile.  See the Payflow Pro Recurring Billing Guide for more details:
-      # https://www.paypal.com/en_US/pdf/PayflowPro_RecurringBilling_Guide.pdf
-      #
-      # Several options are available to customize the recurring profile:
-      #
-      # * <tt>profile_id</tt> - is only required for editing a recurring profile
-      # * <tt>starting_at</tt> - takes a Date, Time, or string in mmddyyyy format. The date must be in the future.
-      # * <tt>name</tt> - The name of the customer to be billed.  If not specified, the name from the credit card is used.
-      # * <tt>periodicity</tt> - The frequency that the recurring payments will occur at.  Can be one of
-      # :bimonthly, :monthly, :biweekly, :weekly, :yearly, :daily, :semimonthly, :quadweekly, :quarterly, :semiyearly
-      # * <tt>payments</tt> - The term, or number of payments that will be made
-      # * <tt>comment</tt> - A comment associated with the profile
-      def recurring(money, credit_card, options = {})
-        options[:name] = credit_card.name if options[:name].blank? && credit_card
-        request = build_recurring_request(options[:profile_id] ? :modify : :add, money, options) do |xml|
-          add_credit_card(xml, credit_card) if credit_card
-        end
-        commit(request, :recurring)
-      end
-
-      def void(authorization, options = {})
-        request = build_reference_request(:void, nil, authorization, options)
-        commit(request)
-      end
-
-      def cancel_recurring(profile_id)
-        request = build_recurring_request(:cancel, 0, :profile_id => profile_id)
-        commit(request, :recurring)
-      end
-
-      def recurring_inquiry(profile_id, options = {})
-        request = build_recurring_request(:inquiry, nil, options.update( :profile_id => profile_id ))
-        commit(request, :recurring)
-      end
-
-      def express
-        @express ||= PayflowExpressGateway.new(@options)
-      end
-
-      private
-
-      def build_reference_sale_or_authorization_request(action, money, reference, options)
-        xml = Builder::XmlMarkup.new
-        xml.tag! TRANSACTIONS[action] do
-          xml.tag! 'PayData' do
-            xml.tag! 'Invoice' do
-              xml.tag! 'TotalAmt', amount(money), 'Currency' => options[:currency] || currency(money)
-            end
-            xml.tag! 'Tender' do
-              xml.tag! 'Card' do
-                xml.tag! 'ExtData', 'Name' => 'ORIGID', 'Value' =>  reference
-              end
-            end
-          end
-        end
-        xml.target!
-      end
-
-      def build_credit_card_request(action, money, credit_card, options)
-        xml = Builder::XmlMarkup.new
-        xml.tag! TRANSACTIONS[action] do
-          xml.tag! 'PayData' do
-            xml.tag! 'Invoice' do
-              xml.tag! 'CustIP', options[:ip] unless options[:ip].blank?
-              xml.tag! 'InvNum', options[:order_id].to_s.gsub(/[^\w.]/, '') unless options[:order_id].blank?
-              xml.tag! 'Description', options[:description] unless options[:description].blank?
-
-              billing_address = options[:billing_address] || options[:address]
-              add_address(xml, 'BillTo', billing_address, options) if billing_address
-              add_address(xml, 'ShipTo', options[:shipping_address], options) if options[:shipping_address]
-
-              xml.tag! 'TotalAmt', amount(money), 'Currency' => options[:currency] || currency(money)
-            end
-
-            xml.tag! 'Tender' do
-              add_credit_card(xml, credit_card)
-            end
-          end
-        end
-        xml.target!
-      end
-
-      def add_credit_card(xml, credit_card)
-        xml.tag! 'Card' do
-          xml.tag! 'CardType', credit_card_type(credit_card)
-          xml.tag! 'CardNum', credit_card.number
-          xml.tag! 'ExpDate', expdate(credit_card)
-          xml.tag! 'NameOnCard', credit_card.first_name
-          xml.tag! 'CVNum', credit_card.verification_value if credit_card.verification_value?
-
-          if requires_start_date_or_issue_number?(credit_card)
-            xml.tag!('ExtData', 'Name' => 'CardStart', 'Value' => startdate(credit_card)) unless credit_card.start_month.blank? || credit_card.start_year.blank?
-            xml.tag!('ExtData', 'Name' => 'CardIssue', 'Value' => format(credit_card.issue_number, :two_digits)) unless credit_card.issue_number.blank?
-          end
-          xml.tag! 'ExtData', 'Name' => 'LASTNAME', 'Value' =>  credit_card.last_name
-        end
-      end
-
-      def credit_card_type(credit_card)
-        return '' if card_brand(credit_card).blank?
-
-        CARD_MAPPING[card_brand(credit_card).to_sym]
-      end
-
-      def startdate(creditcard)
-        year  = format(creditcard.start_year, :two_digits)
-        month = format(creditcard.start_month, :two_digits)
-
-        "#{month}#{year}"
-      end
-
-      def build_recurring_request(action, money, options)
-        unless RECURRING_ACTIONS.include?(action)
-          raise StandardError, "Invalid Recurring Profile Action: #{action}"
-        end
-
-        xml = Builder::XmlMarkup.new
-        xml.tag! 'RecurringProfiles' do
-          xml.tag! 'RecurringProfile' do
-            xml.tag! action.to_s.capitalize do
-              unless [:cancel, :inquiry].include?(action)
-                xml.tag! 'RPData' do
-                  xml.tag! 'Name', options[:name] unless options[:name].nil?
-                  xml.tag! 'TotalAmt', amount(money), 'Currency' => options[:currency] || currency(money)
-                  xml.tag! 'PayPeriod', get_pay_period(options)
-                  xml.tag! 'Term', options[:payments] unless options[:payments].nil?
-                  xml.tag! 'Comment', options[:comment] unless options[:comment].nil?
-
-
-                  if initial_tx = options[:initial_transaction]
-                    requires!(initial_tx, [:type, :authorization, :purchase])
-                    requires!(initial_tx, :amount) if initial_tx[:type] == :purchase
-
-                    xml.tag! 'OptionalTrans', TRANSACTIONS[initial_tx[:type]]
-                    xml.tag! 'OptionalTransAmt', amount(initial_tx[:amount]) unless initial_tx[:amount].blank?
-                  end
-
-                  xml.tag! 'Start', format_rp_date(options[:starting_at] || Date.today + 1 )
-                  xml.tag! 'EMail', options[:email] unless options[:email].nil?
-
-                  billing_address = options[:billing_address] || options[:address]
-                  add_address(xml, 'BillTo', billing_address, options) if billing_address
-                  add_address(xml, 'ShipTo', options[:shipping_address], options) if options[:shipping_address]
-                end
-                xml.tag! 'Tender' do
-                  yield xml
-                end
-              end
-              if action != :add
-                xml.tag! "ProfileID", options[:profile_id]
-              end
-              if action == :inquiry
-                xml.tag! "PaymentHistory", ( options[:history] ? 'Y' : 'N' )
-              end
-            end
-          end
-        end
-      end
-
-      def get_pay_period(options)
-        requires!(options, [:periodicity, :bimonthly, :monthly, :biweekly, :weekly, :yearly, :daily, :semimonthly, :quadweekly, :quarterly, :semiyearly])
-        case options[:periodicity]
-          when :weekly then 'Weekly'
-          when :biweekly then 'Bi-weekly'
-          when :semimonthly then 'Semi-monthly'
-          when :quadweekly then 'Every four weeks'
-          when :monthly then 'Monthly'
-          when :quarterly then 'Quarterly'
-          when :semiyearly then 'Semi-yearly'
-          when :yearly then 'Yearly'
-        end
-      end
-
-      def format_rp_date(time)
-        case time
-          when Time, Date then time.strftime("%m%d%Y")
-        else
-          time.to_s
-        end
-      end
-
-
-'''
 
 TRANSACTIONS = dict(
         purchase       = 'Sale',
