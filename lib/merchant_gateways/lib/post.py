c7eaa25a955b18f8eabc2f0c482588e2715104e0
kmooney <kevin@cukerinteractive.com>
2011-07-08 13:08:42 -0500

    utf8 encode the post values

--- lib/merchant_gateways/lib/post.py
+++ lib/merchant_gateways/lib/post.py
@@ -6,6 +6,8 @@ def post(url, data, headers={}):
     # print 'nononono' TODO  fault if this calls in test mode
 
     if not getattr(data, 'split', False):  #  TODO  take the if out
+        for key, value in data.iteritems():
+            data[key] = value.encode('utf-8')
         data = urllib.urlencode(data)
 
     request = urllib2.Request(url=url, data=data, headers=headers)
